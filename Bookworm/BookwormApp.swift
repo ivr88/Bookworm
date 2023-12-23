07fb72953b665d2dc3fb2fabb08386b63b8a8523
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-12-23 18:12:42 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Вадим Исламов on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
